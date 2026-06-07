8980a4e9cfe0a95f3614d0edc7e920df78d4cc16
TheVeryStarlk <45287860+TheVeryStarlk@users.noreply.github.com>
2026-06-08 00:35:35 +0300

    Add resilience handler

--- Sndr.Client/SndrClientOptions.cs
+++ Sndr.Client/SndrClientOptions.cs
@@ -3,4 +3,10 @@
 public sealed class SndrClientOptions
 {
     public string? Key { get; set; }
+
+    public int MaximumAttempts { get; init; } = 3;
+
+    public TimeSpan BaseDelay { get; init; } = TimeSpan.FromMilliseconds(250);
+
+    public TimeSpan MaximumDelay { get; init; } = TimeSpan.FromMilliseconds(500);
 }
--- Sndr.Client/SndrClientServiceCollectionExtensions.cs
+++ Sndr.Client/SndrClientServiceCollectionExtensions.cs
@@ -1,6 +1,8 @@
-﻿using System.Net.Http.Headers;
+﻿using System.Net;
+using System.Net.Http.Headers;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
+using Polly;
+using Polly.Retry;
 
 namespace Sndr.Client;
 
@@ -9,19 +11,34 @@ public static class SndrClientServiceCollectionExtensions
     public static IServiceCollection AddSndrClient(this IServiceCollection services, Action<SndrClientOptions> configure)
     {
         // Probably should validate the options too?
-        services.Configure(configure);
+        var options = new SndrClientOptions();
 
-        services.AddHttpClient(
-            nameof(SndrClient),
-            static (services, client) =>
-            {
-                var options = services.GetRequiredService<IOptions<SndrClientOptions>>();
+        configure(options);
 
-                // Think of a friendly way for API versioning. 
-                client.BaseAddress = new Uri("https://api.sndr.sh/v1/");
+        services
+            .AddHttpClient(
+                nameof(SndrClient),
+                client =>
+                {
+                    // Think of a friendly way for API versioning. 
+                    client.BaseAddress = new Uri("https://api.sndr.sh/v1/");
 
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", options.Value.Key);
-            });
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", options.Key);
+                })
+            .AddResilienceHandler(
+                nameof(SndrClient),
+                builder => builder.AddRetry(new RetryStrategyOptions<HttpResponseMessage>
+                {
+                    ShouldHandle = new PredicateBuilder<HttpResponseMessage>()
+                        .Handle<HttpRequestException>()
+                        .HandleResult(response => response.StatusCode is HttpStatusCode.TooManyRequests or >= HttpStatusCode.InternalServerError),
+
+                    MaxRetryAttempts = options.MaximumAttempts,
+                    Delay = options.BaseDelay,
+                    BackoffType = DelayBackoffType.Exponential,
+                    UseJitter = true,
+                    MaxDelay = options.MaximumDelay
+                }));
 
         services.AddTransient<SndrClient>();
 
