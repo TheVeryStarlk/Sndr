99bf45989aa3750de98dd5eb4402ee2c446bf1a9
TheVeryStarlk <45287860+TheVeryStarlk@users.noreply.github.com>
2026-06-09 20:17:17 +0300

    Implement domain deletion

---
+++ Sndr.Client/Domains/Deleting/DeleteDomainRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace Sndr.Client.Domains.Deleting;
+
+public sealed class DeleteDomainRequest
+{
+    public required string Identifier { get; init; }
+}
--- Sndr.Client/Domains/SndrClient.cs
+++ Sndr.Client/Domains/SndrClient.cs
@@ -1,10 +1,17 @@
-﻿using Sndr.Client.Domains.Getting;
+﻿using Sndr.Client.Domains.Deleting;
+using Sndr.Client.Domains.Getting;
 using Sndr.Client.Domains.Verification;
 
 namespace Sndr.Client;
 
 public sealed partial class SndrClient
 {
+    public async Task DeleteDomainAsync(DeleteDomainRequest request, CancellationToken cancellationToken = default)
+    {
+        var client = clientFactory.CreateClient(nameof(SndrClient));
+        await client.DeleteAsync($"domains/{Uri.EscapeDataString(request.Identifier)}", cancellationToken);
+    }
+    
     public async Task<DomainResponse?> GetDomainsAsync(CancellationToken cancellationToken = default)
     {
         var client = clientFactory.CreateClient(nameof(SndrClient));
